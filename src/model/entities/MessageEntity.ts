77501f9d4e66f20e75089186338c2e633faa1941
Alexandre Hervé <alexandre.herve@useoptic.com>
2023-10-22 12:48:59 +0200

    Answer to task 3

--- src/model/entities/MessageEntity.ts
+++ src/model/entities/MessageEntity.ts
@@ -8,16 +8,34 @@ export class MessageEntity extends AbstractEntity {
     public readonly emailId: number,
     public readonly body: string,
     public readonly date: Date,
-    id?: number
+    id?: number,
   ) {
     super(id);
   }
 
-  public static createFromEmail(senderId: number | null, threadId: number, email: EmailEntity): MessageEntity {
+  // Note: in a real case scenario I'd benchmark existing libraries to do this: nothing
+  // I'll come up with quickly will cover all the edge cases that a library such as
+  // https://www.npmjs.com/package/string-strip-html does.
+  public static stripHtml(text: string): string {
+    // Naive approach (copied from StackOverflow), just remove anything between '<' and '>'.
+    return text.replace(/(<([^>]+)>)/g, "");
+  }
+
+  public static createFromEmail(
+    senderId: number | null,
+    threadId: number,
+    email: EmailEntity,
+  ): MessageEntity {
     if (!email.id) {
       throw new Error("Email must have an id to be converted to a message");
     }
 
-    return new MessageEntity(senderId, threadId, email.id!, email.body, email.date);
+    return new MessageEntity(
+      senderId,
+      threadId,
+      email.id!,
+      this.stripHtml(email.body),
+      email.date,
+    );
   }
 }
